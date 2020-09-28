5c96b83e2cf0353208fe7cbe9598ae14486f4f11
Lycolia <lycolia@lycolia.info>
2020-09-29 02:16:55 +0900

    add exit code

--- src/libs/exitCodes.js
+++ src/libs/exitCodes.js
@@ -1,27 +1,32 @@
 exports.exitCode = {
   ok: { code: 0, subject: 'OK', message: 'OK' },
-  cdNotExists: {
+  cancelledCreatePj: {
     code: 1,
+    subject: 'Project creation cancelled',
+    message: 'Be retry create project',
+  },
+  cdNotExists: {
+    code: 2,
     subject: 'Current directory may does not exist',
     message: 'Be retry in existing directory',
   },
   existsDistPath: {
-    code: 2,
+    code: 3,
     subject: 'Project directory already exists',
     message: 'Please specify another project name',
   },
   invalidArg: {
-    code: 3,
+    code: 4,
     subject: 'Invalid argument',
     message: 'usage: tsg [project-name]',
   },
   failPull: {
-    code: 4,
+    code: 5,
     subject: 'Failure pull repository.',
     message: '',
   },
   failCreatePj: {
-    code: 5,
+    code: 6,
     subject: 'Failure create project.',
     message: '',
   },
