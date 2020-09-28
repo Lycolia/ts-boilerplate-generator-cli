4e1b750ea1250d44cb27c46362bb41ec37f9ec90
Lycolia <lycolia@lycolia.info>
2020-09-29 01:55:57 +0900

    add warn, change log color

--- src/libs/log.js
+++ src/libs/log.js
@@ -3,8 +3,19 @@
 const chalk = require('chalk');
 
 exports.infoLog = (...message) => {
-  console.info(chalk.yellow('[TS-gen]'), chalk.green(message));
+  console.info(chalk.bgGreen.black('[TS-gen]'), '[INFO]', chalk.green(message));
+};
+exports.warnLog = (...message) => {
+  console.warn(
+    chalk.bgGreen.black('[TS-gen]'),
+    chalk.yellow('[WARN]'),
+    message
+  );
 };
 exports.errorLog = (...message) => {
-  console.error(chalk.yellow('[TS-gen]'), chalk.red('[ERROR]'), message);
+  console.error(
+    chalk.bgGreen.black('[TS-gen]'),
+    chalk.bgRed.white('[ERROR]'),
+    message
+  );
 };
