b30ee7bdf0c86414e35de25d6114c6193369e80c
Lycolia Rizzim <lycolia@lycolia.info>
2021-02-12 08:41:30 +0900

    fix log format

--- src/libs/Log.ts
+++ src/libs/Log.ts
@@ -3,14 +3,14 @@ import chalk from 'chalk';
 /**
  * log banner
  */
-const logBanner = '[TS-gen]';
+const logBanner = ' TS-gen ';
 
 /**
  * put infomation log
  * @param messages
  */
 export const infoLog = (...messages: unknown[]) => {
-  console.info(chalk.bgGreen.black(logBanner), chalk.white('[INFO]'), messages);
+  console.info(chalk.bgGreen.black(logBanner), chalk.white(' INFO '), messages);
 };
 
 /**
@@ -20,7 +20,7 @@ export const infoLog = (...messages: unknown[]) => {
 export const warnLog = (...messages: unknown[]) => {
   console.warn(
     chalk.bgGreen.black(logBanner),
-    chalk.yellow('[WARN]'),
+    chalk.yellow(' WARN '),
     messages
   );
 };
@@ -32,7 +32,7 @@ export const warnLog = (...messages: unknown[]) => {
 export const errorLog = (...messages: unknown[]) => {
   console.error(
     chalk.bgGreen.black(logBanner),
-    chalk.bgRed.white('[ERROR]'),
+    chalk.bgRed.white(' ERROR '),
     messages
   );
 };
