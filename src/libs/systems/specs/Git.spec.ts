37153449b44fc372da9462491ee0750528d75dbe
Lycolia Rizzim <lycolia@lycolia.info>
2021-03-14 17:23:21 +0900

    add multiple platform testing for git

--- src/libs/systems/specs/Git.spec.ts
+++ src/libs/systems/specs/Git.spec.ts
@@ -1,22 +1,88 @@
+import { getTestingPlatform } from '../../TestUtil';
 import * as git from '../Git';
 
 jest.spyOn(git, 'validateInstalled');
 jest.spyOn(git, 'canCommiting');
 
-describe('validateInstalled', () => {
-  it('function can work', () => {
-    git.validateInstalled();
+const platform = getTestingPlatform();
 
-    expect(git.validateInstalled).toBeCalled();
-    expect(git.validateInstalled).toReturn();
+describe('validateInstalled', () => {
+  const testCaseItems = {
+    development: {
+      name: 'development',
+      test() {
+        git.validateInstalled();
+
+        expect(git.validateInstalled).toBeCalled();
+        expect(git.validateInstalled).toReturn();
+      },
+    },
+    only_node: {
+      name: 'only-node',
+      test() {
+        expect(() => {
+          git.validateInstalled();
+        }).toThrow();
+        expect(git.validateInstalled).toBeCalled();
+      },
+    },
+    node_git: {
+      name: 'node-git',
+      test() {
+        git.validateInstalled();
+
+        expect(git.validateInstalled).toBeCalled();
+        expect(git.validateInstalled).toReturn();
+      },
+    },
+    node_git_conf: {
+      name: 'node-git-conf',
+      test() {
+        git.validateInstalled();
+
+        expect(git.validateInstalled).toBeCalled();
+        expect(git.validateInstalled).toReturn();
+      },
+    },
+  };
+
+  it('function can work', () => {
+    testCaseItems[platform].test();
   });
 });
 
 describe('canCommiting', () => {
-  it('function can work', () => {
-    git.canCommiting();
+  const testCaseItems = {
+    development: {
+      name: 'development',
+      test() {
+        expect(git.canCommiting()).toBe(true);
+      },
+    },
+    only_node: {
+      name: 'only-node',
+      test() {
+        expect(() => {
+          git.canCommiting();
+        }).toThrow();
+        expect(git.canCommiting).toBeCalled();
+      },
+    },
+    node_git: {
+      name: 'node-git',
+      test() {
+        expect(git.canCommiting()).toBe(false);
+      },
+    },
+    node_git_conf: {
+      name: 'node-git-conf',
+      test() {
+        expect(git.canCommiting()).toBe(true);
+      },
+    },
+  };
 
-    expect(git.canCommiting).toBeCalled();
-    expect(git.canCommiting).toReturn();
+  it('function can work', () => {
+    testCaseItems[platform].test();
   });
 });
