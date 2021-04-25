bdf4bcff97744c11c88f04982dbd329d157447dc
Lycolia Rizzim <lycolia@lycolia.info>
2021-04-25 19:06:21 +0900

    reject npm version checker

--- src/libs/systems/Npm.ts
+++ src/libs/systems/Npm.ts
@@ -4,23 +4,6 @@ import { TsgException } from '../../models/TsgException';
 import { infoLog } from '../Log';
 
 /**
- * get npm major version
- * @returns x in x.y.z
- */
-export const getNpmMajorVersion = () => {
-  return Number(execSync('npm -v').toString().split('.')[0]);
-};
-
-/**
- *
- * @param npmVer
- * @returns
- */
-export const isNpmVersion7OrLater = (npmVer: number) => {
-  return npmVer >= 7;
-};
-
-/**
  * install npm modules
  * @param projectDest
  * @throws {TsgException}
@@ -28,16 +11,9 @@ export const isNpmVersion7OrLater = (npmVer: number) => {
 export const installNpmModules = (projectDest: string) => {
   try {
     infoLog('Installing npm modules...');
-    const npmVer = getNpmMajorVersion();
-    if (isNpmVersion7OrLater(npmVer)) {
-      execSync(`cd ${projectDest} && npm ci --legacy-peer-deps`, {
-        stdio: 'ignore',
-      });
-    } else {
-      execSync(`cd ${projectDest} && npm ci`, {
-        stdio: 'ignore',
-      });
-    }
+    execSync(`cd ${projectDest} && npm ci`, {
+      stdio: 'ignore',
+    });
   } catch (error) {
     throw new TsgException(ErrorReasons.failNpmInst, error);
   }
--- src/libs/systems/specs/Npm.spec.ts
+++ src/libs/systems/specs/Npm.spec.ts
@@ -1,49 +1,16 @@
 import { execSync } from 'child_process';
-import {
-  getNpmMajorVersion,
-  installNpmModules,
-  isNpmVersion7OrLater,
-} from '../Npm';
+import { installNpmModules } from '../Npm';
 
 jest.mock('child_process');
 const mockExecSync = execSync as jest.MockedFunction<typeof execSync>;
-
-describe('getNpmMajorVersion', () => {
-  it('get number of head', () => {
-    const ver = getNpmMajorVersion();
-    const rawVer = Number(execSync('npm -v').toString().split('.')[0]);
-    expect(ver).toBe(rawVer);
-  });
-});
-
-describe('isNpmVersion7OrLater', () => {
-  it('7 or later', () => {
-    expect(isNpmVersion7OrLater(7)).toBe(true);
-  });
-  it('6 or before', () => {
-    expect(isNpmVersion7OrLater(6)).toBe(false);
-  });
-});
 
 describe('installNpmModules', () => {
   mockExecSync.mockImplementation((command: string) => Buffer.from(command));
 
   it('check npm version', () => {
-    const npmVer = getNpmMajorVersion();
     installNpmModules('test');
-    expect(mockExecSync).toHaveBeenCalled();
-    if (isNpmVersion7OrLater(npmVer)) {
-      expect(mockExecSync).toHaveBeenNthCalledWith(
-        3,
-        'cd test && npm ci --legacy-peer-deps',
-        {
-          stdio: 'ignore',
-        }
-      );
-    } else {
-      expect(mockExecSync).toHaveBeenNthCalledWith(3, 'cd test && npm ci', {
-        stdio: 'ignore',
-      });
-    }
+    expect(mockExecSync).toHaveBeenCalledWith('cd test && npm ci', {
+      stdio: 'ignore',
+    });
   });
 });
