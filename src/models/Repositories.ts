f5b9d94d7363b6b63494858d6425725e4be1afca
Lycolia Rizzim <lycolia@lycolia.info>
2021-11-26 23:38:39 +0900

    support vite-ts-vanilla

--- src/models/Repositories.ts
+++ src/models/Repositories.ts
@@ -4,8 +4,10 @@ import { ProjectType } from './ProjectOptions';
  * boilerplate repositories
  */
 export const Repositories: { [key in ProjectType]: string } = {
-  'ts-server': 'https://github.com/Lycolia/ts-server-boilerplate.git',
-  'ts-react': 'https://github.com/Lycolia/ts-react-boilerplate.git',
-  'ts-next': 'https://github.com/Lycolia/ts-next-boilerplate.git',
   'ts-cli': 'https://github.com/Lycolia/ts-cli-boilerplate.git',
+  'ts-next': 'https://github.com/Lycolia/ts-next-boilerplate.git',
+  'ts-react': 'https://github.com/Lycolia/ts-react-boilerplate.git',
+  'ts-vite-vanilla':
+    'https://github.com/Lycolia/vanilla-ts-vite-boilerplate.git',
+  'ts-server': 'https://github.com/Lycolia/ts-server-boilerplate.git',
 };
