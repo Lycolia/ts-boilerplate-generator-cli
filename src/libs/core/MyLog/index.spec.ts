e5783692946351d44b4bee53a858946889ca5f30
Lycolia Rizzim <lycolia@lycolia.info>
2023-11-02 09:34:22 +0900

    テスト名の日本語化とNode Testへの対応、漏れていたテストの追加

--- src/libs/core/MyLog/index.spec.ts
+++ src/libs/core/MyLog/index.spec.ts
@@ -1,34 +1,39 @@
+import { deepStrictEqual } from 'node:assert';
+import { describe, it, mock } from 'node:test';
 import { MyLog } from '.';
 
 describe('info', () => {
-  it('function can work', () => {
-    const spyedInfo = jest.spyOn(console, 'info');
-    MyLog.info({ test: 'foo', hoge: 'bar' });
-    expect(spyedInfo.mock.calls[0][2]).toEqual({
+  it('console.log()が引数の回数分、呼ばれ、引数が渡っていること', () => {
+    const mockedInfo = mock.method(console, 'info');
+    MyLog.info({ test: 'foo', hoge: 'bar' }, 'test');
+    deepStrictEqual(mockedInfo.mock.calls[0].arguments[2], {
       test: 'foo',
       hoge: 'bar',
     });
+    deepStrictEqual(mockedInfo.mock.calls[1].arguments[2], 'test');
   });
 });
 
 describe('warn', () => {
-  it('function can work', () => {
-    const spyedWarn = jest.spyOn(console, 'warn');
-    MyLog.warn({ test: 'foo', hoge: 'bar' });
-    expect(spyedWarn.mock.calls[0][2]).toEqual({
+  it('console.warn()が引数の回数分、呼ばれ、引数が渡っていること', () => {
+    const mockedWarn = mock.method(console, 'warn');
+    MyLog.warn({ test: 'foo', hoge: 'bar' }, 'test');
+    deepStrictEqual(mockedWarn.mock.calls[0].arguments[2], {
       test: 'foo',
       hoge: 'bar',
     });
+    deepStrictEqual(mockedWarn.mock.calls[1].arguments[2], 'test');
   });
 });
 
 describe('error', () => {
-  it('function can work', () => {
-    const spyedError = jest.spyOn(console, 'error');
-    MyLog.error({ test: 'foo', hoge: 'bar' });
-    expect(spyedError.mock.calls[0][2]).toEqual({
+  it('console.error()が引数の回数分、呼ばれ、引数が渡っていること', () => {
+    const mockedError = mock.method(console, 'error');
+    MyLog.error({ test: 'foo', hoge: 'bar' }, 'test');
+    deepStrictEqual(mockedError.mock.calls[0].arguments[2], {
       test: 'foo',
       hoge: 'bar',
     });
+    deepStrictEqual(mockedError.mock.calls[1].arguments[2], 'test');
   });
 });
