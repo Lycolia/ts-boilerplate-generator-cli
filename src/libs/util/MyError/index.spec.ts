ad2961d39f0616f1d281374af8f873ecf5abde20
Lycolia Rizzim <lycolia@lycolia.info>
2024-01-02 17:08:00 +0900

    ゴミコードの削除

--- src/libs/util/MyError/index.spec.ts
+++ src/libs/util/MyError/index.spec.ts
@@ -13,28 +13,3 @@ describe('constructor', () => {
     assert.deepStrictEqual(actual.message, 'foo');
   });
 });
-
-describe('test', () => {
-  before(() => {
-    console.log('before');
-  });
-  beforeEach(() => {
-    console.log('beforeEach');
-  });
-  afterEach(() => {
-    console.log('afterEach');
-  });
-  after(() => {
-    console.log('after');
-  });
-
-  it('test 1', () => {
-    assert.strictEqual(1, 1);
-  });
-  it('test 2', () => {
-    assert.strictEqual(2, 2);
-  });
-  it('test 3', () => {
-    assert.strictEqual(3, 3);
-  });
-});
