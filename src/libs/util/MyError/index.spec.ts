cf2de679e0c2405dfb07b6c6a33cad7ad54e4424
Lycolia Rizzim <lycolia@lycolia.info>
2024-01-11 12:56:04 +0900

    エラークラスの見直し（causeプロパティの活用、出力処理の追加）

--- src/libs/util/MyError/index.spec.ts
+++ src/libs/util/MyError/index.spec.ts
@@ -1,4 +1,4 @@
-import { after, before, beforeEach, afterEach, describe, it } from 'node:test';
+import { describe, it } from 'node:test';
 import assert from 'node:assert';
 import { MyError } from '.';
 
@@ -9,7 +9,19 @@ describe('constructor', () => {
     // エラーを継承していること
     assert.ok(actual instanceof Error);
     // プロパティに正しく設定されていること
-    assert.deepStrictEqual(actual.error, 123);
-    assert.deepStrictEqual(actual.message, 'foo');
+    assert.deepStrictEqual(actual.cause, 123);
+    assert.deepStrictEqual(actual.reason, {
+      code: 123,
+      message: 'foo',
+    });
+  });
+
+  it('エラーオブジェクトの中身が文字列展開されること', () => {
+    const actual = new MyError({ code: 123, message: 'foo' }, 123);
+
+    assert.match(
+      `${actual}`,
+      /MyError: foo\n +?at TestContext.<anonymous>[\s\S]+reason[\s\S]+code.+/
+    );
   });
 });
--- src/libs/util/MyError/index.ts
+++ src/libs/util/MyError/index.ts
@@ -1,17 +1,24 @@
+import util from 'node:util';
 import { ErrorReason } from '../../../models/ErrorReasons/type';
 
 export class MyError extends Error {
-  public readonly error: unknown;
   public readonly reason: ErrorReason;
 
   /**
    *
    * @param reason
-   * @param error
+   * @param cause
    */
-  constructor(reason: ErrorReason, error?: unknown) {
+  constructor(reason: ErrorReason, cause?: unknown) {
     super(reason.message);
-    this.error = error;
+    this.name = 'MyError';
+    this.cause = cause;
+
     this.reason = reason;
+  }
+
+  public toString() {
+    // 本来デバッグ用の機能だが、このプログラムがツールであるという特性上、使用することに対する支障はないと考える
+    return util.inspect(this);
   }
 }
