c07301f802bdcbe1461fac2f6f6453f9b8b44fe9
Lycolia Rizzim <lycolia@lycolia.info>
2026-01-27 10:57:13 +0900

    npxがこける問題に対応

--- src/libs/system/Npm/index.ts
+++ src/libs/system/Npm/index.ts
@@ -5,9 +5,12 @@ import { MyError } from '../../util/MyError';
 export namespace Npm {
   export const install = (projectDest: string) => {
     try {
-      execSync(`cd ${projectDest} && npm ci && npx prettier -w package.json`, {
-        stdio: 'ignore'
-      });
+      execSync(
+        `cd ${projectDest} && npm ci && npx -y prettier -w package.json`,
+        {
+          stdio: 'ignore'
+        }
+      );
     } catch (error) {
       throw new MyError(ErrorReasons.failNpmInst, error);
     }
